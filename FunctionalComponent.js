795d46380dca898cbc8b1c6f5153b7b5005a2571
Aneeshwar P <86723458+AneeshwarP@users.noreply.github.com>
2022-05-12 19:26:04 +0530

    Add files via upload

---
+++ FunctionalComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FunctionalComponent = () => {
+    var a = 5;
+    var b = a+5;
+    return (
+        <>
+            <h1>This is a Timer function in React.</h1>
+            <h1>Created by - Aneeshwar.</h1>
+        </>
+    );
+}
+
+export default FunctionalComponent;
